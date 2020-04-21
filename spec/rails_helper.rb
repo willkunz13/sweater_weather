f98182b3b75ba5940839deb78736309a0ada8219
jerry <jerry@jerrys-MBP.attlocal.net>
2020-04-21 10:42:12 -0400

    Add safer env variable protection in vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,5 +74,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 	config.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
+	config.filter_sensitive_data('<WEATHER_API_KEY>') { ENV['WEATHER_API_KEY'] }
+	config.filter_sensitive_data('<IMAGE_API_KEY>') { ENV['IMAGE_API_KEY'] }
   config.configure_rspec_metadata!
 end
