9ab3c8ac8703c4d77bc998208d86a4e0001bbcee
jerry <jerry@jerrys-MBP.attlocal.net>
2020-04-21 11:38:32 -0400

    Refactor user model test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,13 @@ SimpleCov.start 'rails' do
 	add_filter "/app/mailers/application_mailer.rb"
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
